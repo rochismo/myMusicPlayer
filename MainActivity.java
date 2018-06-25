045f7b18c63e58fd967cb99632db2c2216c5783e
benjamin <alexis.platino@gmail.com>
2018-06-25 15:00:45 +0200

    Refactored MainActivity code

--- MainActivity.java
+++ MainActivity.java
@@ -2,14 +2,11 @@ package player.media.com.funcionara;
 
 import android.annotation.SuppressLint;
 import android.app.ListActivity;
-import android.content.Context;
 import android.database.Cursor;
-import android.database.DatabaseUtils;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
@@ -18,11 +15,12 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.io.IOException;
+
 public class MainActivity extends ListActivity {
     private static final int UPDATE_FREQUENCY = 500;
     private static final int STEP_VALUE = 4000;
 
-    private TextView selectedfile = null;
+    private TextView selectedFile = null;
     private SeekBar seekBar = null;
     private MediaPlayer player = null;
     private ImageButton prev = null;
@@ -31,14 +29,12 @@ public class MainActivity extends ListActivity {
     private ImageButton stop = null;
     private ImageButton shuffle = null;
 
-    private MediaCursorAdapter adapter = null;
-
     private boolean isStarted = true;
     private String currentFile = "";
     private boolean isMovingSeekBar = false;
     private final Handler handler = new Handler();
 
-    private final Runnable updatePositinRunnable = new Runnable() {
+    private final Runnable updatePositionRunnable = new Runnable() {
         @Override
         public void run() {
             updatePosition();
@@ -49,34 +45,36 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        selectedfile = (TextView) findViewById(R.id.selecteditem);
-        seekBar = (SeekBar) findViewById(R.id.seekBar);
-        prev = (ImageButton) findViewById(R.id.previous);
-        play = (ImageButton) findViewById(R.id.play);
-        next = (ImageButton) findViewById(R.id.next);
-        stop = (ImageButton) findViewById(R.id.stop);
-        shuffle = (ImageButton) findViewById(R.id.shuffle);
-
-        player = new MediaPlayer();
-        player.setOnCompletionListener(onCompletion);
-        player.setOnErrorListener(onError);
-        seekBar.setOnSeekBarChangeListener(seekBarChanged);
+        init();
 
         @SuppressLint("Recycle") Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.
                 EXTERNAL_CONTENT_URI, null, null, null, null);
 
         if (null != cursor) {
             cursor.moveToFirst();
-            while (cursor.moveToNext()){
-
-            }
-            adapter = new MediaCursorAdapter(this, R.layout.item, cursor);
+            MediaCursorAdapter adapter = new MediaCursorAdapter(this, R.layout.item, cursor);
             setListAdapter(adapter);
             prev.setOnClickListener(OnButtonClick);
             play.setOnClickListener(OnButtonClick);
             next.setOnClickListener(OnButtonClick);
             stop.setOnClickListener(OnButtonClick);
+            shuffle.setOnClickListener(OnButtonClick);
         }
+    }
+
+    private void init() {
+        selectedFile = findViewById(R.id.selecteditem);
+        seekBar = findViewById(R.id.seekBar);
+        prev = findViewById(R.id.previous);
+        play = findViewById(R.id.play);
+        next = findViewById(R.id.next);
+        stop = findViewById(R.id.stop);
+        shuffle = findViewById(R.id.shuffle);
+
+        player = new MediaPlayer();
+        player.setOnCompletionListener(onCompletion);
+        player.setOnErrorListener(onError);
+        seekBar.setOnSeekBarChangeListener(seekBarChanged);
     }
 
     @Override
@@ -88,7 +86,7 @@ public class MainActivity extends ListActivity {
 
     private void startPlay(String file) {
         Log.i("Selected: ", file);
-        selectedfile.setText(file);
+        selectedFile.setText(file);
         seekBar.setProgress(0);
         player.stop();
         player.reset();
@@ -110,7 +108,7 @@ public class MainActivity extends ListActivity {
         player.stop();
         player.reset();
         play.setImageResource(android.R.drawable.ic_media_play);
-        handler.removeCallbacks(updatePositinRunnable);
+        handler.removeCallbacks(updatePositionRunnable);
         seekBar.setProgress(0);
         isStarted = false;
     }
@@ -118,7 +116,7 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        handler.removeCallbacks(updatePositinRunnable);
+        handler.removeCallbacks(updatePositionRunnable);
         player.stop();
         player.reset();
         player.release();
@@ -126,9 +124,9 @@ public class MainActivity extends ListActivity {
     }
 
     private void updatePosition() {
-        handler.removeCallbacks(updatePositinRunnable);
+        handler.removeCallbacks(updatePositionRunnable);
         seekBar.setProgress(player.getCurrentPosition());
-        handler.postDelayed(updatePositinRunnable, UPDATE_FREQUENCY);
+        handler.postDelayed(updatePositionRunnable, UPDATE_FREQUENCY);
     }
 
     private View.OnClickListener OnButtonClick = new View.OnClickListener() {
@@ -137,25 +135,26 @@ public class MainActivity extends ListActivity {
             switch (v.getId()) {
                 case R.id.play: {
                     if (player.isPlaying()) {
-                        handler.removeCallbacks(updatePositinRunnable);
+                        handler.removeCallbacks(updatePositionRunnable);
                         player.pause();
                         play.setImageResource(android.R.drawable.ic_media_play);
-                    } else {
-                        if (isStarted) {
-                            player.start();
-                            play.setImageResource(android.R.drawable.ic_media_pause);
-                            updatePosition();
-                        } else {
-                            startPlay(currentFile);
-                        }
+                        break;
                     }
+                    if (isStarted) {
+                        player.start();
+                        play.setImageResource(android.R.drawable.ic_media_pause);
+                        updatePosition();
+                        break;
+                    }
+                    startPlay(currentFile);
                     break;
                 }
 
                 case R.id.next: {
                     int seekto = player.getCurrentPosition() + STEP_VALUE;
-                    if (seekto > player.getDuration())
+                    if (seekto > player.getDuration()) {
                         seekto = player.getDuration();
+                    }
                     player.pause();
                     player.seekTo(seekto);
                     player.start();
@@ -164,8 +163,9 @@ public class MainActivity extends ListActivity {
 
                 case R.id.previous: {
                     int seekto = player.getCurrentPosition() - STEP_VALUE;
-                    if (seekto < 0)
+                    if (seekto < 0) {
                         seekto = 0;
+                    }
                     player.pause();
                     player.seekTo(seekto);
                     player.start();
@@ -173,7 +173,7 @@ public class MainActivity extends ListActivity {
                 }
 
                 case R.id.stop: {
-                    if (player.isPlaying()){
+                    if (player.isPlaying()) {
                         stopPlay();
                     }
                     break;
